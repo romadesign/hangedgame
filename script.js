1f1d56ab297f1eb9c63ad4e62b6afe5b9971e79e
romadesign <roman247.rg@gmail.com>
2022-11-11 12:53:03 +0100

    new cambios

--- script.js
+++ script.js
@@ -1,62 +1,128 @@
-let palabraAleatorio;
-let aletorioconguiones;
-let cont = 6;
+let palabraAleatorio
+let aletorioconguiones
+let cont = 6
 
 //Abcdario
-const abcdario = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const abcdario = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'ñ',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z'
+]
 // Array de palabras
-const palabras = ["atlantico", "ordenador", "laurel", "plaza", "rueda", "cereza", "petanca",]
+const palabras = [
+  'atlantico',
+  'ordenador',
+  'laurel',
+  'plaza',
+  'rueda',
+  'cereza',
+  'petanca'
+]
 
-const contentAbc = document.getElementById("content_abcdario")
-const palabraDiv = document.getElementById("palabra")
-const reset = document.getElementById("reset")
-// Botones de letras
-var buttons = document.getElementsByClassName('letra');
+let contentAbc = document.getElementById('content_abcdario')
+let palabraDiv = document.getElementById('palabra')
+let reset = document.getElementById('reset')
+let content_gif_happy = document.getElementById('content_gif_happy')
+let content_gif_perdedor = document.getElementById('content_gif_perdedor')
+let buttons = document.getElementsByClassName('letra')
+let content = document.getElementById('content')
+let aleatorioShow = document.getElementById('aleatorioShow')
 
-function generaPalabra() {
-  palabraAleatorio = palabras[Math.floor(Math.random() * palabras.length)];
+function generaPalabra () {
+  palabraAleatorio = palabras[Math.floor(Math.random() * palabras.length)]
   aletorioconguiones = palabraAleatorio.split('')
   console.log(palabraAleatorio)
 }
 
-function pintarGuiones() {
+function pintarGuiones () {
   for (let index = 0; index < aletorioconguiones.length; index++) {
-    palabraDiv.innerHTML += aletorioconguiones[index] = " _ ";
+    palabraDiv.innerHTML += aletorioconguiones[index] = '_'
   }
 }
 
-function inicio() {
-  generaPalabra();
+function inicio () {
+  generaPalabra()
   pintarGuiones()
-  document.getElementById("reset").disabled = true;
-  var letra = ""
+  palabraAleatorioRamdon()
+  // document.getElementById('reset').disabled = true
+  var letra = ''
   for (let index = 0; index < abcdario.length; index++) {
-    letra = abcdario[index];
-    contentAbc.innerHTML += "<button value='" + letra + "' onclick='handleClick(\"" + letra + "\")' class='letra' id='" + letra + "'>" + letra + "</button>";
+    letra = abcdario[index]
+    contentAbc.innerHTML +=
+      "<button value='" +
+      letra +
+      "' onclick='handleClick(\"" +
+      letra +
+      "\")' class='letra' id='" +
+      letra +
+      "'>" +
+      letra +
+      '</button>'
   }
-
 }
 
-function handleClick(element) {
-  document.getElementById(element).disabled = true; //desactivando boton  
-  document.getElementById(element).style.backgroundColor="#aaa" //cambiando color
-  console.log(aletorioconguiones)
+function handleClick (element) {
+  document.getElementById(element).disabled = true //desactivando boton cuando ya esta seleccionado
+  document.getElementById(element).style.backgroundColor = '#aaa' //cambiando color del boton cuando ya esta seleccionado
+
   if (palabraAleatorio.indexOf(element) != -1) {
     for (var i = 0; i < palabraAleatorio.length; i++) {
-      if (palabraAleatorio[i] == element) aletorioconguiones[i] = element;
+      if (palabraAleatorio[i] == element) aletorioconguiones[i] = element
     }
-    palabraDiv.innerHTML = aletorioconguiones.join("");
+    palabraDiv.innerHTML = aletorioconguiones.join('') //Quitando comas
+    validacionGanador()
   } else {
-    cont--;
-    document.getElementById("intentos").innerHTML = cont;
-    document.getElementById("image" + cont).className += "showImg";
-    if (cont == 0) {
-      for (let index = 0; index < abcdario.length; index++) {
-        document.getElementById(abcdario[index]).disabled = true;
-      }
-    }
+    cont--
+    document.getElementById('intentos').innerHTML = cont
+    document.getElementById('image' + cont).className += 'showImg'
+    validacionPerdedor()
   }
 }
 
+function validacionPerdedor () {
+  if (cont == 0) {
+    content_gif_perdedor.style.display = 'block'
+    content.style.display = 'none'
+  }
+}
 
-inicio();
+function validacionGanador () {
+  if (aletorioconguiones.join('') == palabraAleatorio) {
+    content_gif_happy.style.display = 'block'
+    content.style.display = 'none'
+  }
+}
+
+function palabraAleatorioRamdon(){
+  const random = palabraAleatorio.split('')
+  aleatorioShow.innerHTML = `" ${random.sort().join(' ')} "` //muestra las letras aleatoriamente Con Join se quitan las comas
+  console.log(random)
+
+
+}
+
+inicio()
